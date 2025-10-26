d2908020c560ff577595f1f674d9e7c4a71d5511
Aziz Rmadi <azizrmadi@gmail.com>
2025-10-26 10:26:12 +0100

    added implementation of string parser

--- Main.hs
+++ Main.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use lambda-case" #-}
 module Main where
 
 data JsonValue
@@ -9,7 +12,41 @@ data JsonValue
   | JsonObject [(String, JsonValue)]
   deriving (Show, Eq)
 
-type Parser a = String -> Maybe (a, String)
+newtype Parser a = Parser
+  { runParser :: String -> Maybe (String, a)
+  }
+
+instance Functor Parser where
+  fmap f p =
+    Parser
+      ( \input -> case runParser p input of
+          Nothing -> Nothing
+          Just (restInp, val) -> Just (restInp, f val)
+      )
+
+instance Applicative Parser where
+  pure x = Parser (\input -> Just (input, x))
+  (Parser p1) <*> (Parser p2) =
+    Parser
+      ( \input -> do
+          (input', f) <- p1 input
+          (input'', val) <- p2 input'
+          pure (input'', f val)
+      )
+
+jsonNull :: Parser JsonValue
+jsonNull = undefined
+
+charP :: Char -> Parser Char
+charP x = Parser f
+  where
+    f (y : ys)
+      | y == x = Just (ys, x)
+      | otherwise = Nothing
+    f [] = Nothing
+
+stringP :: String -> Parser String
+stringP = traverse charP
 
 jsonValue :: Parser JsonValue
 jsonValue = undefined
